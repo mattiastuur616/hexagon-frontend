814c984297dee80eefda73212df2533b58fa6074
Mattias Tüür <mattiastuur@gmail.com>
2024-05-12 11:32:27 +0300

    Books have now an intro animation.

--- src/containers/library/Library.jsx
+++ src/containers/library/Library.jsx
@@ -18,67 +18,85 @@ const Library = () => {
 
       <div className='hex__library-container'>
         <div className='hex__library-container_group'>
-          <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
-            <Book 
-            imgUrl={chapter1} 
-            chapter="1" 
-            title="The Protector of The Realm" />
+          <motion.div initial={{ x: 150, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0, type: "tween" }}>
+            <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
+              <Book 
+              imgUrl={chapter1} 
+              chapter="1" 
+              title="The Protector of The Realm" />
+            </motion.div>
           </motion.div>
 
-          <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
-            <Book 
-            imgUrl={chapter2} 
-            chapter="2" 
-            title="Between The Unreal" />
+          <motion.div initial={{ x: 150, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0.2, type: "tween" }}>
+            <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
+              <Book 
+              imgUrl={chapter2} 
+              chapter="2" 
+              title="Between The Unreal" />
+            </motion.div>
           </motion.div>
 
-          <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
-            <Book 
-            imgUrl={chapter3} 
-            chapter="3" 
-            title="The World of Tomorrow" />
+          <motion.div initial={{ x: 150, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0.4, type: "tween" }}>
+            <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
+              <Book 
+              imgUrl={chapter3} 
+              chapter="3" 
+              title="The World of Tomorrow" />
+            </motion.div>
           </motion.div>
 
-          <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
-            <Book 
-            imgUrl={chapter4} 
-            chapter="4" 
-            title="No Time to Sleep" />
+          <motion.div initial={{ x: 150, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0.6, type: "tween" }}>
+            <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
+              <Book 
+              imgUrl={chapter4} 
+              chapter="4" 
+              title="No Time to Sleep" />
+            </motion.div>
           </motion.div>
 
-          <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
-            <Book 
-            imgUrl={chapter5} 
-            chapter="5" 
-            title="The Maze in The Storm" />
+          <motion.div initial={{ x: 150, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0.8, type: "tween" }}>
+            <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
+              <Book 
+              imgUrl={chapter5} 
+              chapter="5" 
+              title="The Maze in The Storm" />
+            </motion.div>
           </motion.div>
 
-          <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
-            <Book 
-            imgUrl={chapter6} 
-            chapter="6" 
-            title="The Project Unspeakable" />
+          <motion.div initial={{ x: 150, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 1, type: "tween" }}>
+            <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
+              <Book 
+              imgUrl={chapter6} 
+              chapter="6" 
+              title="The Project Unspeakable" />
+            </motion.div>
           </motion.div>
 
-          <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
-            <Book 
-            imgUrl={chapter7} 
-            chapter="7" 
-            title="The Risk of Necromancer" />
+          <motion.div initial={{ x: 150, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 1.2, type: "tween" }}>
+            <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
+              <Book 
+              imgUrl={chapter7} 
+              chapter="7" 
+              title="The Risk of Necromancer" />
+            </motion.div>
           </motion.div>
 
-          <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
-            <Book 
-            imgUrl={chapter8} 
-            chapter="8" 
-            title="Timelines of The Parasyte" />
+          <motion.div initial={{ x: 150, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 1.4, type: "tween" }}>
+            <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
+              <Book 
+              imgUrl={chapter8} 
+              chapter="8" 
+              title="Timelines of The Parasyte" />
+            </motion.div>
           </motion.div>
 
-          <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
-            <Book 
-            imgUrl={chapter9} 
-            chapter="9" 
-            title="The Death Congress" />
+          <motion.div initial={{ x: 150, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 1.6, type: "tween" }}>
+            <motion.div whileHover={{ scale:1.1, boxShadow: "0px 0px 16px rgb(255,251,0)" }}>
+              <Book 
+              imgUrl={chapter9} 
+              chapter="9" 
+              title="The Death Congress" />
+            </motion.div>
           </motion.div>
         </div>
       </div>
